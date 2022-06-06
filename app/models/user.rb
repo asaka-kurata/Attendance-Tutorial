86e4030b42031f84d01087c836802eeb274b3a79
asaka-kurata <knightofpen.ta.cles6@gmail.com>
2022-06-06 23:56:11 +0000

    4.4検証終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.emai = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 100 },format: {with: VALID_EMAIL_REGEX }, uniquenss: true
