d1b467d5c7761be9e207da7842791bb4c69b1b32
asaka-kurata <knightofpen.ta.cles6@gmail.com>
2022-06-07 00:24:47 +0000

    4.5パスワード追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 100 },format: {with: VALID_EMAIL_REGEX }, uniquenss: true
+  has_secure_password
+  validates :passwore:, presence: true, length: {minimum: 6 }
 end
