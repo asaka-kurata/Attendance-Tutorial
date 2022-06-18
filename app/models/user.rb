c59673a37e2acdf4f96fb8041fb3e5d86cb9cf8b
asaka-kurata <knightofpen.ta.cles6@gmail.com>
2022-06-18 17:38:17 +0000

    Finish full-fledged-login

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: 100 }, format: {with: VALID_EMAIL_REGEX }, uniqueness: true
+  validates :email, presence: true, length: { maximum: 100 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
   
@@ -34,6 +34,13 @@ class User < ApplicationRecord
   
   # トークンがダイジェストと一致すればtrueを返します。
   def authenticated?(remember_token)
+    # ダイジェストが存在しない場合はfalseを返して終了します。
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  # ユーザーのログイン情報を破棄します。
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
