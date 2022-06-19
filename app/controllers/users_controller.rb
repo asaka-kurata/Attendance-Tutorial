69d5603fe07fa61b2d2702f6e405fac9719f2be4
asaka-kurata <knightofpen.ta.cles6@gmail.com>
2022-06-19 12:53:56 +0000

    8. 2 認可機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,21 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
   end
   
   def new
-    @user = User.new # ユーザーオブジェクトを生成し、インスタンス変数に代入します。
+    @user = User.new
   end
   
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user # 保存成功後、ログインします。
-      flash[:success] = '新規登録に成功しました。'
+      log_in @user
+      flash[:success] = '新規作成に成功しました。'
       redirect_to @user
     else
       render :new
@@ -37,5 +40,25 @@ class UsersController < ApplicationController
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    # beforeフィルター
+    
+    # paramsハッシュからユーザーを取得します。
+    def set_user
+      @user = User.find(params[:id])
+    end
+    
+    # ログイン済みのユーザーか確認します。
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+    end
+    
+    def correct_user
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
