e1c674e54ab4d6ba1a69a115aee0524e32010789
asaka-kurata <knightofpen.ta.cles6@gmail.com>
2022-06-20 10:33:12 +0000

    8.3終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   before_action :correct_user, only: [:edit, :update]
   
   def show
-    @user = User.find(params[:id])
   end
   
   def new
@@ -23,11 +22,9 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
